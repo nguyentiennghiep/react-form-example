cad9173d5706fc87d38eaccb42fce6235f54c444
nghiepnguyentien94 <nguyen_tien_nghiep@mediado.jp>
2018-06-11 22:23:34 +0700

    test data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,40 @@ import Control from './components/Control';
 import TaskList from './components/TaskList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: [] };
+  }
+
+  onGenerateData = () => {
+    var tasks = [
+      {
+        id: this.generateID(),
+        name: "Learn Viet",
+        status: true
+      },
+      {
+        id: this.generateID(),
+        name: "Learn Thai",
+        status: false
+      },
+      {
+        id: this.generateID(),
+        name: "Learn English",
+        status: true
+      }
+    ];
+    this.setState({task : tasks});
+  }
+
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
+  generateID() {
+    return this.s4()+this.s4()+'-'+this.s4()+this.s4()+'-'+this.s4()+this.s4()+'-'+this.s4()+this.s4()+this.s4();
+  }
+
   render() {
     return (
       <div className="container">
@@ -17,12 +51,18 @@ class App extends Component {
             <TaskForm />
           </div>
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-            <button type="button" className="btn btn-primary">
-              <span className="fa fa-plus mr-5"></span>Add work
+            <button type="button" className="btn btn-primary mr-5">
+              Add work
+                </button>
+
+            <button type="button"
+              className="btn btn-danger ml-5"
+              onClick={this.onGenerateData} >
+              Generate Data
                 </button>
             <Control />
             <div className="row mt-15">
-             <TaskList/>
+              <TaskList />
             </div>
           </div>
         </div>
