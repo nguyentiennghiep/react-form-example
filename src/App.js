4adeeaeb221e1a39863b1cc343d7d65e44c5de86
nguyen tien nghiep <nguyen_tien_nghiep@mediado.local>
2018-06-11 19:01:45 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
 
 class App extends Component {
   render() {
@@ -11,72 +13,13 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <div className="panel panel-warning">
-              <div className="panel-heading">
-                <h3 className="panel-title">Add a work</h3>
-              </div>
-              <div className="panel-body">
-                <form>
-                  <div className="form-group">
-                    <label>Name :</label>
-                    <input type="text" className="form-control" />
-                  </div>
-                  <label>Status :</label>
-                  <select className="form-control" required="required">
-                    <option value="1">Active</option>
-                    <option value="0">Deactive</option>
-                  </select>
-                  <br />
-                  <div className="text-center">
-                    <button type="submit" className="btn btn-warning">Add</button>&nbsp;
-                                <button type="submit" className="btn btn-danger">Cancel</button>
-                  </div>
-                </form>
-              </div>
-            </div>
+            <TaskForm />
           </div>
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
             <button type="button" className="btn btn-primary">
               <span className="fa fa-plus mr-5"></span>Add work
                 </button>
-            <div className="row mt-15">
-              <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Nhập từ khóa..." />
-                  <span className="input-group-btn">
-                    <button className="btn btn-primary" type="button">
-                      <span className="fa fa-search mr-5"></span>Search
-                            </button>
-                  </span>
-                </div>
-              </div>
-              <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div className="dropdown">
-                  <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    Filter <span className="fa fa-caret-square-o-down ml-5"></span>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li>
-                      <a role="button">
-                        <span className="fa fa-sort-alpha-asc pr-5">
-                          Name A-Z
-                                                </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a role="button">
-                        <span className="fa fa-sort-alpha-desc pr-5">
-                          Name Z-A
-                                                </span>
-                      </a>
-                    </li>
-                    <li role="separator" className="divider"></li>
-                    <li><a role="button">Status : Active</a></li>
-                    <li><a role="button">Status : Deactive</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            <Control />
             <div className="row mt-15">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <table className="table table-bordered table-hover">
